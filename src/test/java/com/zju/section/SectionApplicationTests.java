4eaba077e2dd11524ac9d7bba1e3d61a02758320
CYH <cyh050415@gmail.com>
2025-05-23 14:11:21 +0800

    Init springboot

---
+++ src/test/java/com/zju/section/SectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zju.section;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
